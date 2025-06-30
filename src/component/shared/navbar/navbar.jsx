f261cb8dbe55bb8f4a75afb416fa7e102b6a98c5
taz <tazahmedsoft@gmail.com>
2025-07-01 01:27:10 +0600

    modify navbar

--- src/component/shared/navbar/navbar.jsx
+++ src/component/shared/navbar/navbar.jsx
@@ -96,14 +96,16 @@ const Navbar = () => {
             <div className="dropdown dropdown-end">
               <div tabIndex={0} className="btn btn-ghost btn-circle avatar">
                 <div className="w-10 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                  <img src={userInFo?.imageUrl} alt="profile" />
+                  <img src={userInFo?.user?.imageUrl} alt="profile" />
                 </div>
               </div>
               <ul
                 tabIndex={0}
                 className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-white text-black rounded-box w-52"
               >
-                <li className="px-4 py-2 font-semibold">{userInFo?.name}</li>
+                <li className="px-2.5 py-2 font-semibold">
+                  {userInFo?.user?.name}
+                </li>
                 <li onClick={handleLogOut}>
                   <a>Logout</a>
                 </li>
--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://event-management-server-jade.vercel.app" }),
   endpoints: () => ({}),
-  tagTypes: ["event"],
+  tagTypes: ["event"]
+  
 });
